4935c9bb4ef11303ca6ca4939ff668078d29a0d5
ge72vap <ge72vap@mytum.de>
2024-07-23 21:28:55 +0200

    typo fix

--- project/google_cloud_solution/terraform/application/providers.tf
+++ project/google_cloud_solution/terraform/application/providers.tf
@@ -1,11 +1,11 @@
 provider "google" {
-    reqion                      = "eu-west3"
+    region                      = "eu-west3"
     zone                        = "eu-west3-b"
     impersonate_service_account = "service-account@${var.project}.iam.gserviceaccount.com"
 }
 
 provider "google-beta" {
-    reqion                      = "eu-west3"
+    region                      = "eu-west3"
     zone                        = "eu-west3-b"
     impersonate_service_account = "service-account@${var.project}.iam.gserviceaccount.com"
 }
