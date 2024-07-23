dda70f4645ec131d27e783a7d7cf278913242187
ge72vap <ge72vap@mytum.de>
2024-07-23 20:41:05 +0200

    test

--- project/google_cloud_solution/terraform/application/providers.tf
+++ project/google_cloud_solution/terraform/application/providers.tf
@@ -1,28 +1,11 @@
 provider "google" {
     reqion                      = "eu-west3"
     zone                        = "eu-west3-b"
-    access_token                = data.google_service_account_access_token.default.access_token
-    request_timeout 	        = "60s"
+    impersonate_service_account = "service-account@${var.project}.iam.gserviceaccount.com"
 }
 
 provider "google-beta" {
     reqion                      = "eu-west3"
     zone                        = "eu-west3-b"
-    access_token                = data.google_service_account_access_token.default.access_token
-    request_timeout 	        = "60s"
-}
-
-provider "google" {
- alias = "impersonation"
- scopes = [
-   "https://www.googleapis.com/auth/cloud-platform",
-   "https://www.googleapis.com/auth/userinfo.email",
- ]
-}
-
-data "google_service_account_access_token" "default" {
- provider               	= google.impersonation
- target_service_account 	= "${var.service_account_app}@${var.project}.iam.gserviceaccount.com"
- scopes                 	= ["userinfo-email", "cloud-platform"]
- lifetime               	= "1200s"
+    impersonate_service_account = "service-account@${var.project}.iam.gserviceaccount.com"
 }
